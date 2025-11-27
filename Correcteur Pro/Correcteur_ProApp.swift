570ae4c3ca6cc31bc16147010d09b0e089744a24
Elvispressselait <hadrien.rose@protonmail.com>
2025-11-27 23:44:40 +0100

    Initial Commit

---
+++ Correcteur Pro/Correcteur_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Correcteur_ProApp.swift
+//  Correcteur Pro
+//
+//  Created by Hadrien Rose on 27/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Correcteur_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
